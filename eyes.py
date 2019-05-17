99c7f414354656021fd24ce3ab3507589a7189d9
Samuel Martin <martinsa0182@btinternet.com>
2019-05-17 11:41:51 +0100

    Reverted lots of changes to eye code. Should run fine with majimboo's py-mathutils

--- eyes.py
+++ eyes.py
@@ -1,26 +1,23 @@
-from pyquaternion import Quaternion
 import numpy as np
-from math import degrees
+from mathutils import Vector, Quaternion
 
 
 class Eyes:
 
-    def __init__(self, cross_eye=0, default_look_distance=200, eye_fov=40):
-        self.default_look_target = np.array((0, default_look_distance, 0))
+    def __init__(self, cross_eye=0.0, default_look_distance=200, eye_fov=40):
+        self.default_look_target = Vector((0, default_look_distance, 0))
         self.look_target = self.default_look_target
         self.rotation = Quaternion((1, 0, 0, 0))
-        self.leye_pos = np.array((-5.96898, 6.09201, 3.69949))
-        self.reye_pos = np.array((5.96898, 6.09201, 3.69949))
-        self.leye_norm = np.array((cross_eye, 1, 0))
-        self.reye_norm = np.array((-cross_eye, 1, 0))
-        self.pupil_pos = [0, 0, 0, 0]  # lx ly rx ry
+        self.position = Vector((0, 0, 0))
+        self.leye_pos = Vector((-5.96898, 6.09201, 3.69949))
+        self.reye_pos = Vector((5.96898, 6.09201, 3.69949))
+        self.leye_norm = Vector((cross_eye, 1, 0))
+        self.reye_norm = Vector((-cross_eye, 1, 0))
+        self.pupil_pos = np.zeros(4)  # lx ly rx ry
         self.eye_fov = eye_fov
 
     def set_rotation(self, rotation):
         self.rotation = rotation
-
-    def get_pupil_pos(self):
-        return self.eye_fov
 
     def update(self):
         self.update_pupil_pos()
@@ -36,45 +33,44 @@ class Eyes:
     def update_pupil_pos(self):
 
         leye_pos = self.leye_pos.copy()
-        leye_pos = self.rotation.rotate(leye_pos)
+        leye_pos.rotate(self.rotation)
+        leye_pos += self.position
 
         reye_pos = self.reye_pos.copy()
-        reye_pos = self.rotation.rotate(reye_pos)
+        reye_pos.rotate(self.rotation)
+        reye_pos += self.position
 
         leye_beam = self.look_target.copy() - leye_pos
         reye_beam = self.look_target.copy() - reye_pos
 
-        leye_beam = self.rotation.inverse.rotate(leye_beam)
+        leye_beam.rotate(self.rotation.inverted())
         leye_beam_angle = self.leye_norm.rotation_difference(leye_beam)
 
-        reye_beam = self.rotation.inverse.rotate(reye_beam)
+        reye_beam.rotate(self.rotation.inverted())
         reye_beam_angle = self.reye_norm.rotation_difference(reye_beam)
 
-        self.pupil_pos = [-1 * degrees(leye_beam_angle.z),
-                          -1 * degrees(leye_beam_angle.x),
-                          -1 * degrees(reye_beam_angle.z),
-                          -1 * degrees(reye_beam_angle.x)
-                          ]
+        self.pupil_pos = -1*np.degrees([leye_beam_angle.z, leye_beam_angle.x, reye_beam_angle.z, reye_beam_angle.x])
 
     def update_look_target(self, forced=False):
         fov = np.full(4, self.eye_fov) / np.array([2, 4, 2, 4])
 
         if forced or np.any(self.pupil_pos < -1*fov) or np.any(self.pupil_pos > fov):
-            new_look_target = self.default_look_target
+            new_look_target = self.default_look_target.copy()
             new_look_target.rotate(self.rotation)
+            new_look_target += self.position
             return new_look_target
         else:
             return False
 
 
-
 if __name__ == "__main__":
+
+    e = Eyes(cross_eye=-.1)
+
+    import bpy
     for i in range(300):
-        import bpy
 
         bpy.context.scene.frame_set(i)
-
-        e = Eyes()
 
         e.set_rotation(bpy.data.objects['imu'].rotation_quaternion)
 
